c8731ea1d7de2aa47369cc915b5ffec218ded0b3
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-11-08 14:53:23 +0200

    added updated videoPlayer and videoRecorder

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import '@qonsoll/react-design/dist/styles/vars/index.css'
 import '@qonsoll/react-design/dist/styles/styles.css'
 import './styles/vars.css'
+import '@qonsoll/qvideo/dist/styles/styles.css'
 
 ReactDOM.render(
   <Router>
